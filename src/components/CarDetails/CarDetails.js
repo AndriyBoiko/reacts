00343ff8d48d0bf53cfda9b02a4da887dc2baf68
AndriyBoiko <talisman.andriy@gmail.com>
2023-07-23 11:17:55 +0300

    models to brand

--- src/components/CarDetails/CarDetails.js
+++ src/components/CarDetails/CarDetails.js
@@ -4,7 +4,7 @@ import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom"
 import {carActions} from "../../redux";
 
-const CarDetails = ({car, car: {id, model, price, year}}) => {
+const CarDetails = ({car, car: {id, brand, price, year}}) => {
     console.log(car)
     const dispatch = useDispatch();
     const navigate = useNavigate()
@@ -18,7 +18,7 @@ const CarDetails = ({car, car: {id, model, price, year}}) => {
             <div className={css.card}>
                 <h3 className={css['car-details']}>Car details</h3>
                 <p><span>Id:</span> {id}</p>
-                <p><span>Model:</span> {model}</p>
+                <p><span>Model:</span> {brand}</p>
                 <p><span>Price: </span>{price} $</p>
                 <p><span>Year:</span> {year}</p>
                 <button className={css['btn']} onClick={deleteById}>delete</button>
